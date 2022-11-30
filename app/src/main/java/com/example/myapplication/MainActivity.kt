227cbe9ec1b9233690489a93767472715b28699c
EslamShaban74 <eslamshaban12>
2022-11-30 14:09:45 +0200

    onClick & setOnClickListener

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // btn.setOnClickListener {
+        //  Toast.makeText(this,"hello",Toast.LENGTH_LONG).show()
+        // }
+    }
+
+    fun btnClick(view: View) {
+        Toast.makeText(this, "hello", Toast.LENGTH_LONG).show()
+    }
+
+    fun press(view: View) {
+        if (view == tv) {
+            Toast.makeText(this, "Hello text view", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "hello button", Toast.LENGTH_LONG).show()
+        }
     }
 }
